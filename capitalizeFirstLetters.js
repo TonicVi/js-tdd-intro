e7f3d9293ea398b477de7de5c9f1020993285743
Vi <dupre.virginie@gmail.com>
2020-01-13 11:24:12 +0100

    tests created

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,13 +1,49 @@
-const assert = require('assert');
+const assert = require("assert");
 
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
+function capitalizeFirstLetters(input) {
+}
+  // let output = "";
+  // let words = input.split(" ");
+  // console.log(words);
+  // if (words.length === 1) {
+  //   for (let i = 0; i < input.length; i++) {
+  //     if (i === 0) {
+  //       output += input[0].toUpperCase();
+  //     } else {
+  //       output += input[i];
+  //     }
+  //   }
+  //   return output;
+  // } else if (words.length > 1) {
+  //   for (let i = 0; i < words[i].length; i++) {
+  //     console.log(words[i].length)
+  //     for (let j = 0; j < words[i][j].length; j++) {
+  //       words[i]
+
+  //     }
+
+      // for (let j = 0; j < words[i].length; j++) 
+      
+      //   if (j === 0) {
+      //     output += words[0].toUpperCase();
+      //   } else {
+      //     output += words[j];
+      //   }
+      // }}}
+
+
+
+assert.strictEqual(typeof capitalizeFirstLetters, "function");
 
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 
-assert.strictEqual(capitalizeFirstLetters('lalala'), 'Lalala');
+assert.strictEqual(capitalizeFirstLetters("lalala"), "Lalala");
 
-assert.strictEqual(capitalizeFirstLetters('get to the chopper'), 'Get To The Chopper');
+assert.strictEqual(
+  capitalizeFirstLetters("get to the chopper"),
+  "Get To The Chopper"
+);
 
-assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
+assert.strictEqual(capitalizeFirstLetters("z"), "Z");
 
-assert.strictEqual(capitalizeFirstLetters(''), '');
+assert.strictEqual(capitalizeFirstLetters(""), "");
