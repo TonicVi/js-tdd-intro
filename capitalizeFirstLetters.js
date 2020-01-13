fe8a47649b729084888165ea7b81074d44f2d9b7
Vi <dupre.virginie@gmail.com>
2020-01-13 11:48:48 +0100

    installing mocha

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,24 +1,9 @@
-const assert = require("assert");
-
 function capitalizeFirstLetters(input) {
-    return input
-    .split(' ')
-    .map( w =>  w.substring(0,1).toUpperCase()+ w.substring(1))
-    .join(' ')
+  let words = "";
+  return (words = input
+    .split(" ")
+    .map(w => w.substring(0, 1).toUpperCase() + w.substring(1))
+    .join(" "));
 }
 
-
-assert.strictEqual(typeof capitalizeFirstLetters, "function");
-
-assert.strictEqual(capitalizeFirstLetters.length, 1);
-
-assert.strictEqual(capitalizeFirstLetters("lalala"), "Lalala");
-
-assert.strictEqual(
-  capitalizeFirstLetters("get to the chopper"),
-  "Get To The Chopper"
-);
-
-assert.strictEqual(capitalizeFirstLetters("z"), "Z");
-
-assert.strictEqual(capitalizeFirstLetters(""), "");
+module.exports = capitalizeFirstLetters;
